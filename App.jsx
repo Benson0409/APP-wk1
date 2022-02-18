fe6b777f358f734e3fafc57fa0c48f76ef120e62
Benson <benson5508@gmail.com>
2022-02-18 13:58:39 +0800

    畫面文字編輯

--- App.jsx
+++ App.jsx
@@ -4,7 +4,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world 123</Text>
+      
+      <Text>數位二甲</Text>
+      <Text>簡柏松(Benson)</Text>
+      <Text>110919019</Text>
+      <Text>有事歡迎聯絡我</Text>
       <StatusBar style="auto" />
     </View>
   );
